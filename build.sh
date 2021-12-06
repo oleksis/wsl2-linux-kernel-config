a9c8e11846024de25fdd165853366f63ae2857f6
oleksis <oleksis.fraga@gmail.com>
2021-12-05 22:16:00 -0500

    Comment requirements. Set fail on errors

--- build.sh
+++ build.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 
+# Requirements for Debian/Ubuntu
+#sudo apt install -y git bc build-essential flex bison libssl-dev libelf-dev dwarves
+#sudo apt install -y curl jq
+
+# Requirements for openSUSE
+#sudo zypper in -y -t pattern devel_basis
+#sudo zypper in -y bc openssl openssl-devel dwarves rpm-build libelf-devel
+#sudo zypper in -y curl jq
+
+# Fail on errors.
+set -e
+
 mkdir -p linux
 pushd linux
-
-sudo apt install -y git bc build-essential flex bison libssl-dev libelf-dev dwarves
-sudo apt install -y curl jq
 
 linux_json="$(curl -s https://api.github.com/repos/torvalds/linux/tags | jq -r '.[0]')"
 linux_name="$(echo $linux_json | jq -r '.name')"
 
+# echo $linux_name | sed 's/$/.tar.gz/' | sed 's#^#https://github.com/torvalds/linux/archive/refs/tags/#' | wget -c -i -
 echo $linux_json | jq -r '.tarball_url' | wget -c -i -
 
-echo "Untar $linux_name..."
+echo -n "Untar $linux_name..."
 tar -xf "$linux_name"
+echo ""
+
 cd "$(find -maxdepth 1 -type d -regex '\.\/.*linux-.*')"
 
-echo "Copy custom default config..."
-cp ../../Microsoft/config-wsl .config
+echo -n "Copy custom default config..."
+cp -f ../../Microsoft/config-wsl .config
+echo ""
 
 #SECONDS=0
 _start=$SECONDS
 
 make olddefconfig
-##make silentoldconfig
+#make silentoldconfig
 make -j4
 
 _elapsedseconds=$(( SECONDS - _start ))
