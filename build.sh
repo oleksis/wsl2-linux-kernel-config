e55716f15597648992cf89259abe052620348841
Oleksis Fraga <oleksis.fraga@gmail.com>
2023-02-20 03:41:45 -0500

    Build using git shallow-clone by default

--- build.sh
+++ build.sh
@@ -3,13 +3,10 @@
 # https://www.leemhuis.info/files/misc/How%20to%20quickly%20build%20a%20trimmed%20Linux%20kernel%20%E2%80%94%20The%20Linux%20Kernel%20documentation.html
 
 # Requirements for Debian/Ubuntu
-#sudo apt install -y git bc build-essential flex bison libssl-dev libelf-dev dwarves
-#sudo apt install -y curl jq wget
+#sudo apt install -y git bc build-essential flex bison openssl libssl-dev libelf-dev dwarves
+#sudo apt install -y binutils gcc make pahole perl-base
 
-# Requirements for openSUSE
-#sudo zypper in -y -t pattern devel_basis
-#sudo zypper in -y bc openssl openssl-devel dwarves rpm-build libelf-devel
-#sudo zypper in -y curl jq wget
+# [Install build requirements](https://www.leemhuis.info/files/misc/How%20to%20quickly%20build%20a%20trimmed%20Linux%20kernel%20%E2%80%94%20The%20Linux%20Kernel%20documentation.html#install-build-requirements)
 
 # Requirements for Windows
 # Windows Powershell CLI
@@ -20,49 +17,52 @@ set -e
 
 mkdir -p linux
 pushd linux
-rm -rf *linux-* v*
 
-file_ext=".tar.gz"
+linux_url="https://github.com/torvalds/linux.git"
 
-if [[ -z $1 ]]; then
-	linux_json="$(curl -s https://api.github.com/repos/torvalds/linux/tags | jq -r '.[0]')"
-	linux_name="$(echo $linux_json | jq -r '.name')"
-	# echo $linux_name | sed 's/$/.tar.gz/' | sed 's#^#https://github.com/torvalds/linux/archive/refs/tags/#' | wget -c -i -
-	linux_url="$(echo $linux_json | jq -r '.tarball_url')"
-else
-	linux_tag=$1
-	linux_name=linux-$linux_tag
-	linux_url="https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/snapshot/${linux_name}${file_ext}"
+if [ "$1" == "kernel" ]; then
+	linux_url="https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git"
 fi
 
-file_name="${linux_name}${file_ext}"
-
-curl -L -C - -o ${file_name} $linux_url
-
-echo -n "Untar ${file_name}..."
-tar -xf "$file_name"
+echo -n "Housekeeping..."
+rm -rf ./build
 echo ""
 
-# find -maxdepth 1 -type d -name '*linux-*'
-cd "$(find -maxdepth 1 -type d -regex '\.\/.*linux-.*')"
+if [[ ! -d ./sources ]]; then
+	mkdir -p ./sources ./build
+	echo "Clonning kernel..."
+	git clone --depth 1 -b master ${linux_url} ./sources
+	cd sources/
+else
+	mkdir ./build
+	cd sources/
+	echo "Feching kernel..."
+	git fetch --depth 1
+	git checkout --force --detach origin/master
+fi
+
+# ➜ du -hs sources/
+# 1.8G    sources/
+#
+# ➜ du -hs torvalds-linux-53b3c64/
+# 5.5G    torvalds-linux-53b3c64/
+
+linux_version=v$(make -s kernelversion)
 
 echo -n "Copy custom default config..."
-cp -f ../../Microsoft/config-wsl .config
+cp -f ../../Microsoft/config-wsl ../build/.config
 echo ""
 
-#SECONDS=0
 _start=$SECONDS
 
-make olddefconfig
-#make silentoldconfig
-make -j4
+make O=../build/ olddefconfig
+make -j $(nproc --all) O=../build/
 
 _elapsedseconds=$(( SECONDS - _start ))
 TZ=UTC0 printf 'Kernel builded: %(%H:%M:%S)T\n' "$_elapsedseconds"
 echo "Kernel build finished: $(date -u '+%H:%M:%S')"
 
-powershell.exe /C 'Copy-Item -Force .\arch\x86\boot\bzImage $env:USERPROFILE\bzImage-'$linux_name
-powershell.exe /C 'Write-Output [wsl2]`nkernel=$env:USERPROFILE\bzImage-'$linux_name' | % {$_.replace("\","\\")} | Out-File $env:USERPROFILE\.wslconfig -encoding ASCII'
+powershell.exe /C 'Copy-Item -Force ..\build\arch\x86\boot\bzImage $env:USERPROFILE\bzImage-'$linux_version
+powershell.exe /C 'Write-Output [wsl2]`nkernel=$env:USERPROFILE\bzImage-'$linux_version' | % {$_.replace("\","\\")} | Out-File $env:USERPROFILE\.wslconfig -encoding ASCII'
 
 popd
-
